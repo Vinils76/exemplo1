73ec19c92f23ebb1a394a7319c53ce6ae85006a3
Vinils76 <antoniovinicius7670@gmail.com>
2022-11-04 11:58:48 -0300

    Create estilos.js

---
+++ estilos.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  um: {
+    width: "100%",
+    backgroundColor: "salmon",
+    flex: 2,
+  },
+  dois: {
+    width: "100%",
+    backgroundColor: "lightskyblue",
+    flex: 2,
+  },
+  titulo: {
+    color: "purple",
+    fontSize: 32,
+  },
+  subtitulo: {
+    color: "#f00",
+    fontSize: 24,
+  },
+});
+
+export default estilos;
